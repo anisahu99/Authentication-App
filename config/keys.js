b38f93ba0862a5d4cd4ec716501dc8d8039d9eae
Animesh Sahu <72248977+anisahu99@users.noreply.github.com>
2023-12-17 18:18:53 +0530

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,17 @@
+module.exports={
+    google:{
+        clientID:GOOGLE_CLIENT_ID,
+        clientSecret:GOOGLE_CLIENT_SECRET
+    },
+    mongodb:{
+        dbURI:MONGODB_URL
+    },
+    session: {
+        cookieKey: SESSION_SECRET
+    },
+    github:{
+        clientID: GITHUB_CLIENT_ID,
+        clientSecret: GITHUB_CLIENT_SECRET
+      }
+
+};
